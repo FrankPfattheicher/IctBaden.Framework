67ad7a9c39ca1f7de3b627eccce059d02e423414
Frank Pfattheicher <fpf@ict-baden.de>
2019-05-03 10:54:27 +0200

    Added NetworkInfo

---
+++ IctBaden.Framework/Network/NetworkInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace IctBaden.Framework.Network
+{
+    using System.Net;
+    using System.Net.Sockets;
+
+    public static class NetworkInfo
+    {
+        public static int GetFreeLocalTcpPort()
+        {
+            var tcpListener = new TcpListener(IPAddress.Loopback, 0);
+            tcpListener.Start();
+            var port = ((IPEndPoint)tcpListener.LocalEndpoint).Port;
+            tcpListener.Stop();
+            return port;
+        }
+
+        public static int GetFreeLocalUdpPort()
+        {
+            var port = -1;
+            using (var udpClient = new UdpClient(0))
+            {
+                port = ((IPEndPoint)udpClient.Client.LocalEndPoint).Port;
+                udpClient.Close();
+            }
+            return port;
+        }
+
+    }
+}
