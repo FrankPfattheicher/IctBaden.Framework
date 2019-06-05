9eb74c1973b44f37def66206db5fcf31b31a4132
Frank Pfattheicher <fpf@ict-baden.de>
2019-06-05 17:32:16 +0200

    CPU and memory info implementation for linux

--- IctBaden.Framework/AppUtils/CpuInfoLinux.cs
+++ IctBaden.Framework/AppUtils/CpuInfoLinux.cs
@@ -1,10 +1,65 @@
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable IdentifierTypo
+// ReSharper disable CommentTypo
+
 namespace IctBaden.Framework.AppUtils
 {
     public class CpuInfoLinux : ICpuInfo
     {
+        private const string StatInfo = "/proc/stat";
+
+        private ulong _prevCpuTime;
+        private ulong _prevCpuIdle;
+
         public float GetCpuUsage()
         {
-            throw new System.NotImplementedException();
+            if (!File.Exists(StatInfo))
+            {
+                Trace.TraceError($"System file {StatInfo} does not exist.");
+                return 0.0f;
+            }
+            
+            var info = File.ReadLines(StatInfo).FirstOrDefault() ?? "";
+            var numbers = Regex.Split(info, @"\D+", RegexOptions.Singleline)
+                .Where(n => !string.IsNullOrWhiteSpace(n))
+                .ToArray();
+
+            // https://github.com/Leo-G/DevopsWiki/wiki/How-Linux-CPU-Usage-Time-and-Percentage-is-calculated
+            var user = ulong.Parse(numbers[0]);
+            var nice = ulong.Parse(numbers[1]);
+            var system = ulong.Parse(numbers[2]);
+            var idle = ulong.Parse(numbers[3]);
+            var iowait = ulong.Parse(numbers[4]);
+            var irq = ulong.Parse(numbers[5]);
+            var softirq = ulong.Parse(numbers[6]);
+            var steal = ulong.Parse(numbers[7]);
+//            var guest = ulong.Parse(numbers[8]);
+//            var guest_nice = ulong.Parse(numbers[9]);
+            
+            // Total CPU time since boot = user+nice+system+idle+iowait+irq+softirq+steal
+            var totalCpuTime = user + nice + system + idle + iowait + irq + softirq + steal;
+
+            // Total CPU Idle time since boot = idle + iowait
+            var totalCpuIdle = idle + iowait;
+
+            var currentTime = totalCpuTime - _prevCpuTime; 
+            var currentIdle = totalCpuIdle - _prevCpuIdle;
+
+            _prevCpuTime = totalCpuTime;
+            _prevCpuIdle = totalCpuIdle;
+            
+            // Total CPU usage time since boot = Total CPU time since boot - Total CPU Idle time since boot
+            var currentUsage = currentTime - currentIdle;
+
+            // Total CPU percentage = Total CPU usage time since boot/Total CPU time since boot X 100
+            var totalCpuPercentage = currentUsage * 100.0f / currentTime;
+
+            return totalCpuPercentage;
         }
     }
 }
--- IctBaden.Framework/AppUtils/MemoryInfoLinux.cs
+++ IctBaden.Framework/AppUtils/MemoryInfoLinux.cs
@@ -1,25 +1,63 @@
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
 namespace IctBaden.Framework.AppUtils
 {
     public class MemoryInfoLinux : IMemoryInfo
     {
+        private const string MemInfo = "/proc/meminfo";
+
+        private ulong _ramTotalBytes;
+        private ulong _ramAvailableBytes;
+        private ulong _virtualTotalBytes;
+        private ulong _virtualAvailableBytes;
+        
+        
+        private void ParseMemInfo()
+        {
+            if (!File.Exists(MemInfo))
+            {
+                Trace.TraceError($"System file {MemInfo} does not exist.");
+                return;
+            }
+            
+            var info = File.ReadAllText(MemInfo);
+            var memTotal = new Regex(@"MemTotal\:\s+(\w+)\s+").Match(info);
+            _ramTotalBytes = (memTotal.Success) ? ulong.Parse(memTotal.Groups[1].Value) : 0;
+
+            var memAvailable = new Regex(@"MemAvailable\:\s+(\w+)\s+").Match(info);
+            _ramAvailableBytes = (memAvailable.Success) ? ulong.Parse(memAvailable.Groups[1].Value) : 0;
+            
+            var virtualTotal = new Regex(@"SwapTotal\:\s+(\w+)\s+").Match(info);
+            _virtualTotalBytes = (virtualTotal.Success) ? ulong.Parse(virtualTotal.Groups[1].Value) : 0;
+
+            var virtualAvailable = new Regex(@"SwapFree\:\s+(\w+)\s+").Match(info);
+            _virtualAvailableBytes = (virtualAvailable.Success) ? ulong.Parse(virtualAvailable.Groups[1].Value) : 0;
+        }
+        
         public ulong GetSystemRamInstalled()
         {
-            throw new System.NotImplementedException();
+            ParseMemInfo();
+            return _ramTotalBytes;
         }
 
         public float GetSystemRamUsage()
         {
-            throw new System.NotImplementedException();
+            ParseMemInfo();
+            return (_ramTotalBytes - _ramAvailableBytes) * 100.0f / _ramTotalBytes;
         }
 
         public ulong GetSystemTotalVirtualMemory()
         {
-            throw new System.NotImplementedException();
+            ParseMemInfo();
+            return _virtualTotalBytes;
         }
 
         public float GetSystemVirtualMemoryUsage()
         {
-            throw new System.NotImplementedException();
+            ParseMemInfo();
+            return (_virtualTotalBytes - _virtualAvailableBytes) / (float)_virtualTotalBytes;
         }
     }
 }
