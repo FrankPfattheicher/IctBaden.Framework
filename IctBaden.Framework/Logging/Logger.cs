d9793688f710f72b9b0625509d7e8e5b2e0d3c3c
Frank Pfattheicher <fpf@ict-baden.de>
2020-12-14 11:58:54 +0100

    Add Logging support using Microsoft.Extensions.Logging.

---
+++ IctBaden.Framework/Logging/Logger.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace IctBaden.Framework.Logging
+{
+    public static class Logger
+    {
+        /// <summary>
+        /// Tries to find static field of type ILoggerFactory in entry assembly.
+        /// If not found return simple factory for console and tron output with default configuration.
+        /// </summary>
+        public static ILoggerFactory DefaultFactory
+        {
+            get
+            {
+                // find static field of type ILoggerFactory in entry assembly
+                var entry = Assembly.GetEntryAssembly();
+                ILoggerFactory loggerFactory;
+                foreach (var entryType in entry!.DefinedTypes)
+                {
+                    var fieldInfo = entryType.DeclaredFields.FirstOrDefault(f => f.FieldType == typeof(ILoggerFactory));
+                    loggerFactory = (ILoggerFactory) fieldInfo?.GetValue(null);
+                    if (loggerFactory == null) continue;
+                    
+                    Trace.TraceInformation($"Using LoggerFactory '{fieldInfo.Name}' of type '{entryType.Name}'.");
+                    return loggerFactory;
+                }
+
+                Trace.TraceWarning($"No LoggerFactory found. Using console factory.");
+                var emptyConfiguration = new ConfigurationBuilder().Build();
+                return CreateConsoleAndTronFactory(emptyConfiguration);
+            }
+        }
+
+        /// <summary>
+        /// Creates logger factory for console and tron output with given configuration.
+        /// Configuration settings:
+        /// LogLevel - overall level (LogLevel.Information)
+        /// TronTraceLogLevel - TRON output level (LogLevel.Information)
+        /// </summary>
+        /// <param name="configuration">Configuration to use</param>
+        /// <returns>Logger factory</returns>
+        public static ILoggerFactory CreateConsoleAndTronFactory(IConfiguration configuration)
+        {
+            var minimumLogLevel = configuration.GetValue<LogLevel>("LogLevel", LogLevel.Information);
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.AddProvider(new TronLoggerProvider(configuration));
+                builder.SetMinimumLevel(minimumLogLevel);
+            });
+            return loggerFactory;
+        }
+        
+    }
+}
