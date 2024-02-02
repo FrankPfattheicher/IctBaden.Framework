6b5708a22ca93ef28d1b4efb247712e47af2cc92
Frank Pfattheicher <fpf@ict-baden.de>
2024-02-02 09:46:51 +0100

    ClassLoaderShouldLoadBaseTypes

--- IctBaden.Framework.Test/IniFile/ProfileClassLoaderTests.cs
+++ IctBaden.Framework.Test/IniFile/ProfileClassLoaderTests.cs
@@ -11,6 +11,11 @@ namespace IctBaden.Framework.Test.IniFile;
 
 public class ProfileClassLoaderTests : IDisposable
 {
+    private class TestSuperClass : TestClass
+    {
+        public int SuperInteger { get; set; }
+    }
+
     private class TestClass
     {
         public bool Boolean1 { get; set; }
@@ -121,6 +126,33 @@ public class ProfileClassLoaderTests : IDisposable
         Assert.Equal(new List<string> {"a","b","c"}, testClass.TextList);
         Assert.Equal(new[] {"a","b","c"}, testClass.TextArray);
         Assert.Equal(new List<int> {1,2,3}, testClass.IntList);
+    }
+
+    [Fact]
+    public void ClassLoaderShouldLoadBaseTypes()
+    {
+        using (var fileData = new StreamWriter(_testFileName, false, Encoding.Unicode))
+        {
+            fileData.WriteLine("[Properties]");
+            fileData.WriteLine("Boolean1=1");
+            fileData.WriteLine("BooleanTrue=true");
+            fileData.WriteLine("Integer=123");
+            fileData.WriteLine("SuperInteger=456");
+            fileData.WriteLine("Numeric1=4.567");
+            fileData.WriteLine("Numeric2=4.567");
+            fileData.Close();
+        }
+        var ini = new Profile(_testFileName);
+        var testClass = new TestSuperClass();
+        var loader = new ProfileClassLoader(CultureInfo.InvariantCulture);
+        loader.LoadClass(testClass, ini, "Properties");
+            
+        Assert.True(testClass.Boolean1);
+        Assert.True(testClass.BooleanTrue);
+        Assert.Equal(123, testClass.Integer);
+        Assert.Equal(456, testClass.SuperInteger);
+        Assert.Equal(4.567f, testClass.Numeric1);
+        Assert.Equal(4.567, testClass.Numeric2);
     }
 
 }
